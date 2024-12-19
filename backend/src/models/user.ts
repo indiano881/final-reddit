e6ccaa0be98e3d921d05405dc986995e36fa3760
davidebaldi <d.baldi88@gmail.com>
2024-12-19 12:25:49 +0100

    hashing works

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,4 +1,5 @@
-import {Document, model, Schema} from 'mongoose';
+import {Document, model, MongooseError, Schema} from 'mongoose';
+import bcrypt from 'bcrypt'
 
 type TUser= Document & {//Document _generate _id from mongoose
     username: string,
@@ -24,4 +25,20 @@ const UserSchema= new Schema({
     timestamps: true
 })
 
+UserSchema.pre('save', async function (next) {//CANNOT DO ARROW FUNCTION BECAUSE WE NEED TO USE THIS
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    try {
+        const hashedPassword= await bcrypt.hash(this.password, 10);
+        this.password=hashedPassword
+        
+    } catch (error) {
+        if (error instanceof MongooseError) {
+            next(error)
+        }
+        throw error
+    }
+}) 
 export const User= model<TUser>('User', UserSchema) //1st name of the model/table 2nd the schema structure of it
