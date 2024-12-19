c68b75ff19ade9f1f3226d593922f2d995beb4d8
davidebaldi <d.baldi88@gmail.com>
2024-12-19 12:42:16 +0100

    log in worksgit add .!

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router } from "express";
 import { User } from "../models/user";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const signUp=  async (req: Request, res: Response)=> {
     try {
@@ -22,6 +24,28 @@ const signUp=  async (req: Request, res: Response)=> {
     }
 }
 
+const logIn=  async (req: Request, res: Response)=> {
+    try {
+        const {username, password}= req.body
+
+        if (!username ||!password) res.status(400).json({message: 'username or password missing'})
+
+        const user= await User.findOne({username}, '+password')
+
+        if (!user || !(await bcrypt.compare(password, user.password))) res.status(400).json({message: 'username or password are wrong'})
+
+        const accessToken= jwt.sign({userId: user?._id}, process.env.JWT_SECRET!, {
+            expiresIn: '1h'
+        })
+
+        res.status(200).json({accessToken, userId: user?._id})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send() //must put send otherwise stucks
+    }
+}
+
 export const authRouter= Router();
 
 authRouter.post('/auth/sign-up', signUp)
+authRouter.post('/auth/log-in', logIn)
